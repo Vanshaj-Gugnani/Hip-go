c142f46d71efaec4b21ac9a89e48981efd852264
vgugnani <vgugnani@myseneca.ca>
2024-03-22 19:21:56 -0400

    inital commmit

---
+++ js/artists.js
@@ -0,0 +1,32 @@
+/**
+ * artists.js
+ *
+ * The app's list of Artists
+ */
+
+window.artists = [
+  {
+    artistId: 'AID-51234',
+    name: 'KR$NA',
+    urls: [
+      { url: 'https://www.youtube.com/@KRSNAOfficial', name: 'Youtube' },
+      { url: 'https://www.instagram.com/realkrsna/', name: 'Instagram' },
+    ]
+  },
+  {
+    artistId: 'AID-51235',
+    name: 'RAFTAAR',
+    urls: [
+      { url: 'https://www.youtube.com/@raftaarmusic', name: 'Youtube' },
+      { url: 'https://www.instagram.com/raftaarmusic/', name: 'Instagram' },
+    ]
+  },
+  {
+    artistId: 'AID-51236',
+    name: 'KARMA',
+    urls: [
+      { url: 'https://www.youtube.com/channel/UCN5voRYhro0QDgCkiWW7qMw', name: 'Youtube' },
+      { url: 'https://www.instagram.com/karmathelekhak/', name: 'Instagram' },
+    ]
+  }
+];
